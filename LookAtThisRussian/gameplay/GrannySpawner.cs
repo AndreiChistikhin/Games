58c4e8b54a1e11556f01532ab0ee6088b22c603a
AndreiChistikhin <78159702+AndreiChistikhin@users.noreply.github.com>
2021-05-27 15:17:20 +0700

    Update GrannySpawner.cs

--- LookAtThisRussian/gameplay/GrannySpawner.cs
+++ LookAtThisRussian/gameplay/GrannySpawner.cs
@@ -16,60 +16,60 @@ float minimumSpawnCoordinate = -445f;
 float maximumSpawnCoordinate = 445f;
 
     void Start()
-    {
-		scriptUI = GameObject.FindObjectOfType<UI>().GetComponent<UI>();
-		spawnTimer = gameObject.AddComponent<Timer>();
-		StartRandomTimer();
-	}
+   {
+	scriptUI = GameObject.FindObjectOfType<UI>().GetComponent<UI>();
+	spawnTimer = gameObject.AddComponent<Timer>();
+	StartRandomTimer();
+   }
 
     void Update()
     {
-		if (spawnTimer.Finished)
-		{
-			HandleSpawnTimerFinishedEvent();
-		}
-	}
-
-	private void HandleSpawnTimerFinishedEvent()
+	if (spawnTimer.Finished)
 	{
-		if (GameObject.FindObjectsOfType<Granny>().Length < 7)
-        {
-			GrannySpawn();
-            StartRandomTimer();
-        }
+	   HandleSpawnTimerFinishedEvent();
 	}
+    }
+
+    void HandleSpawnTimerFinishedEvent()
+   {
+	if (GameObject.FindObjectsOfType<Granny>().Length < 7)
+        {
+	   GrannySpawn();
+           StartRandomTimer();
+        }
+    }
 
     //Spawn granny on an edge of the map
 	void GrannySpawn()
     {
         spawnLocation.x = Random.Range(minimumSpawnCoordinate, maximumSpawnCoordinate);
-		spawnLocation.y = 0;
-		spawnLocation.z = Random.Range(minimumSpawnCoordinate, maximumSpawnCoordinate);
-		int i = Random.Range(1, 5);
-		if (i == 1)
-		{
+	spawnLocation.y = 0;
+	spawnLocation.z = Random.Range(minimumSpawnCoordinate, maximumSpawnCoordinate);
+	int i = Random.Range(1, 5);
+	if (i == 1)
+	{
             spawnLocation.x = minimumSpawnCoordinate;
-		}
-		else if (i == 2)
-		{
+	}
+	else if (i == 2)
+	{
             spawnLocation.x = maximumSpawnCoordinate;
         }
-		else if (i == 3)
-		{
+	else if (i == 3)
+	{
             spawnLocation.z = minimumSpawnCoordinate;
         }
-		else 
-		{
+	else 
+	{
             spawnLocation.z = maximumSpawnCoordinate;
         }
 
         Instantiate<GameObject>(prefabGranny, new Vector3(spawnLocation.x, spawnLocation.y, spawnLocation.z), Quaternion.identity);
         scriptUI.GrannyAdded();
-	}
+    }
 
 	void StartRandomTimer()
     {
-		spawnTimer.Duration =Random.Range(25,100);
-		spawnTimer.Run();
-	}
+	spawnTimer.Duration =Random.Range(25,100);
+	spawnTimer.Run();
+    }
 }
